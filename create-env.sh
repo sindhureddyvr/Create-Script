cdb7028afa5fa1238f799a443da480b71f4a91e8
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-09 18:58:10 +0000

    Create Script to add instances,LB,Autoscaling group

---
+++ create-env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo -e "\e[1m Lab4 Create Script"
+echo -e "Creating 3 new instances \e[0m"
+aws ec2 run-instances --image-id $1 --key-name awskey --security-group-ids sg-d5539bac --instance-type t2.micro --user-data file://installapp.sh --count 3 --placement AvailabilityZone=us-west-2a
+echo -e "New Instance creation completed"
+
+echo -e "\e[1mWait untill the Instance are in  Running State\e[0m"
+instance_id=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId]' --filters Name=instance-state-name,Values=pending)
+echo $instance_id
+aws ec2 wait instance-running --instance-ids $instance_id
+echo -e "Wait is completed and instances are now in Running state"
+
+echo -e "\e[1mCreating a Load Balancer\e[0m"
+aws elb create-load-balancer --load-balancer-name itmo-544 --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-1865246e
+echo -e "Load balancer is  created successfully"
+
+echo -e "\e[1mRegistering newly created instances with the load balancer \e[0m"
+aws elb register-instances-with-load-balancer --load-balancer-name itmo-544 --instances $instance_id
+echo -e "Instances are registered to load balancer successfully"
+
+echo -e "\e[1mCreating Autoscaling Launch Configuration\e[0m"
+aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id $1 --key-name awskey --instance-type t2.micro --user-data file://sample.sh
+echo -e "Autoscaling Launch Configuration created successfully"
+
+echo -e "\e[1mCreating Autoscaling Group\e[0m"
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name webserverdemo --launch-configuration webserver --availability-zone us-west-2a --max-size 5 --min-size 0 --desired-capacity 1
+echo -e "AutoScaling Group Created Successfully"
+
+echo -e "\e[1mAttaching created instances to auto scaling group \e[0m"
+aws autoscaling attach-instances --instance-ids $instance_id --auto-scaling-group-name webserverdemo
+echo -e "Instances attached to auto-scaling-group successfully"
+
+echo -e "\e[1mAttaching load balancer to auto scaling group\e[0m"
+aws autoscaling attach-load-balancers --auto-scaling-group-name webserverdemo --load-balancer-names itmo-544
+echo -e "Load balancer attached to auto-scaling-group successfully"
+
