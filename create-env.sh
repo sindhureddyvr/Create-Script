74a1d0dca0a9e29516f607945034fa17ecfa933a
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-10 03:23:22 +0000

    Added validation for parameters

--- create-env.sh
+++ create-env.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+parameters=$#
+
+if [ $# -ne 3 ]
+then
+echo "This script requires 3 parameter to be passed(image-id,key-name,security-group-id).Please pass the right number of parameters and run the script again."
+else
 echo -e "\e[1m Lab4 Create Script"
-echo -e "Creating 3 new instances \e[0m"
-aws ec2 run-instances --image-id $1 --key-name awskey --security-group-ids sg-d5539bac --instance-type t2.micro --user-data file://installapp.sh --count 3 --placement AvailabilityZone=us-west-2a
-echo -e "New Instance creation completed"
+echo -e "Creating 3 micro instances \e[0m"
+aws ec2 run-instances --image-id $1 --key-name $2 --security-group-ids $3 --instance-type t2.micro --user-data file://installapp.sh --count 3 --placement AvailabilityZone=us-west-2a
+echo -e "New Instances are created"
 
 echo -e "\e[1mWait untill the Instance are in  Running State\e[0m"
 instance_id=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId]' --filters Name=instance-state-name,Values=pending)
@@ -15,12 +21,12 @@ echo -e "\e[1mCreating a Load Balancer\e[0m"
 aws elb create-load-balancer --load-balancer-name itmo-544 --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-1865246e
 echo -e "Load balancer is  created successfully"
 
-echo -e "\e[1mRegistering newly created instances with the load balancer \e[0m"
+echo -e "\e[1mRegistering instances with the load balancer \e[0m"
 aws elb register-instances-with-load-balancer --load-balancer-name itmo-544 --instances $instance_id
 echo -e "Instances are registered to load balancer successfully"
 
 echo -e "\e[1mCreating Autoscaling Launch Configuration\e[0m"
-aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id $1 --key-name awskey --instance-type t2.micro --user-data file://sample.sh
+aws autoscaling create-launch-configuration --launch-configuration-name webserver --image-id $1 --key-name $2 --instance-type t2.micro --user-data file://installapp.sh
 echo -e "Autoscaling Launch Configuration created successfully"
 
 echo -e "\e[1mCreating Autoscaling Group\e[0m"
@@ -34,4 +40,4 @@ echo -e "Instances attached to auto-scaling-group successfully"
 echo -e "\e[1mAttaching load balancer to auto scaling group\e[0m"
 aws autoscaling attach-load-balancers --auto-scaling-group-name webserverdemo --load-balancer-names itmo-544
 echo -e "Load balancer attached to auto-scaling-group successfully"
-
+fi
